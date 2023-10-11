2d0ecf5f6d59ab3acd08fc992082bdb27e689cbb
souhir farjallah <souhir_farjallah@live.fr>
2023-10-11 13:35:24 +0200

    finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,46 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+const pokemon = pokemonList[1]
+const isImg = pokemon.imgSrc ? (
+    <img src={pokemon.imgSrc} alt={pokemon.name} />
+  ) : (
+    <p>???</p>
+  );
+
+
     return (
       <figure>
-        <img 
-        src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
+        <img
+          src={pokemon.imgSrc}
+          alt={pokemon.name}
         />
-        <figcaption> 
-        "Bulbasaur"
-        </figcaption>
-      </figure>    
-    );
-  }
+   
+      <figcaption>
+        {pokemon.name}
+      </figcaption>
+      </figure>
+  );
+}
+
+export default PokemonCard;
+
+
+
+
+
+ 
   
-  export default PokemonCard ;
+  
+  
+  
+  
